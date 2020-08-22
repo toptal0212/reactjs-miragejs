d141bce5a5eba1036a8660fef0e2bc20c56628ca
Murayama <proLove0212@gmail.com>
2020-08-22 13:54:33 +0100

    move Users component to same level as other components

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {User} from "./user/User";
+import {User} from "../user/User";
 import {MirageJsServer} from "../../mirageJsServer";
 
 interface IUser {
