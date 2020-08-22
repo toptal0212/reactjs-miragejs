a1c1fc5493e967ab6d4033eb1ee3066e12be3c4a
Murayama <proLove0212@gmail.com>
2020-08-22 14:17:30 +0100

    fix linting issues

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export class Navbar extends React.Component<Props> {
     render(): React.ReactElement {
         return <div className="uk-navbar-container uk-navbar">
             <div className="uk-navbar-center">
-                <a href="" className="uk-navbar-item uk-logo">{this.props.title}</a>
+                <a href="/" className="uk-navbar-item uk-logo">{this.props.title}</a>
             </div>
         </div>
     }
