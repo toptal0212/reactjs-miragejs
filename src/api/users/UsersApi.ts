2c2f05dd80a951a6743a234242b6f6a02a9e5148
Murayama <proLove0212@gmail.com>
2020-08-23 15:16:11 +0100

    add UsersApi tests

--- src/api/users/UsersApi.ts
+++ src/api/users/UsersApi.ts
@@ -1,9 +1,21 @@
 import {IUserJson} from "../../components/users/Users";
 
 export class UsersApi {
+
+    private static checkStatus(response: Response): Promise<Response> {
+        if (response.status >= 200 && response.status < 300) {
+            return Promise.resolve(response);
+        }
+        throw new Error(response.statusText);
+    };
+
     static fetchUsers(): Promise<IUserJson[]> {
         return fetch('/api/users')
+            .then((response) => UsersApi.checkStatus(response))
             .then(response => response.json())
-            .catch(error => console.error('Failed to retrieve users: ', error));
+            .catch(error => {
+                    return Promise.reject('Failed to retrieve users: ' + error)
+                }
+            );
     }
 }
--- src/api/users/__tests__/UsersApiTest.tsx
+++ src/api/users/__tests__/UsersApiTest.tsx
@@ -2,6 +2,11 @@ import {IUserJson} from "../../../components/users/Users";
 import {UsersApi} from "../UsersApi";
 
 describe('UsersApi', () => {
+    function finaliseTest(done: jest.DoneCallback) {
+        jest.resetAllMocks();
+        done();
+    }
+
     function testUsersJson(): IUserJson[] {
         return [
             {
@@ -22,7 +27,15 @@ describe('UsersApi', () => {
         });
     }
 
-    it('renders fetched Users', () => {
+    function mockFailedFetchPromise(users = testUsersJson()) {
+        return Promise.resolve({
+            json: () => Promise.resolve(users),
+            status: 401,
+            statusText: 'Not Authorised Dawg!'
+        })
+    }
+
+    it('retrieves Users and status 200 OK', (done) => {
         jest.spyOn(global, 'fetch').mockImplementation(() => mockSuccessFetchPromise() as Promise<Response>);
 
         UsersApi.fetchUsers()
@@ -30,7 +43,23 @@ describe('UsersApi', () => {
                 users => {
                     expect(fetch).toHaveBeenCalledWith('/api/users');
                     expect(users.length).toEqual(2);
-                    expect(users).toEqual(testUsersJson())
+                    expect(users).toEqual(testUsersJson());
+
+                    finaliseTest(done);
+                }
+            );
+    });
+
+    it('throws an error with an error message when Users retrieval fails', (done) => {
+        jest.spyOn(global, 'fetch').mockImplementation(() => mockFailedFetchPromise() as Promise<Response>);
+
+        UsersApi.fetchUsers()
+            .catch(
+                (error) => {
+                    expect(fetch).toHaveBeenCalledWith('/api/users');
+                    expect(error).toEqual('Failed to retrieve users: Error: Not Authorised Dawg!');
+
+                    finaliseTest(done);
                 }
             );
     });
