6ddd54ad8a3b033a760e17ca1a6120aad4daeaff
Murayama <proLove0212@gmail.com>
2020-08-28 01:04:05 +0100

    Add form to Add Users

--- src/components/user/add_user/AddUser.tsx
+++ src/components/user/add_user/AddUser.tsx
@@ -1,40 +1,62 @@
 import React, {Component} from "react";
+import {Field, Form, Formik} from "formik";
+
 
 interface User {
-    name: string;
-    surname: string;
+    user: {
+        name: string;
+        surname: string;
+    }
 }
 
-export class AddUser extends Component {
+export class AddUser extends Component<any, User> {
 
-    state = {
-        user: {
-            name: '',
-            surname: ''
-        }
-    };
+    constructor(props: any, state: User) {
+        super(props, state);
+        this.state = {
+            user: {
+                name: '',
+                surname: ''
+            }
+        };
+    }
 
     private formTitle = "Add A User";
     private buttonValue = "Add User";
 
     render() {
         return <div>
-            <form>
-                <fieldset className="uk-fieldset">
-                    <legend className="uk-legend">{this.formTitle}</legend>
+            <Formik
+                initialValues={{name: '', surname: ''}}
+                onSubmit={async values => {
+                    await new Promise(resolve => setTimeout(resolve, 500));
+                    this.setState({
+                        user: {
+                                name: values.name,
+                                surname: values.surname
+                            }
+                    })
+                }}
+            >
+                <Form>
+                    <fieldset className="uk-fieldset">
+                        <legend className="uk-legend">{this.formTitle}</legend>
 
-                    <div className="uk-margin">
-                        <input className="uk-input uk-form-width-large" type="text" placeholder="First Name"/>
-                    </div>
-                    <div className="uk-margin">
-                        <input className="uk-input uk-form-width-large" type="text" placeholder="Surname"/>
-                    </div>
+                        <div className="uk-margin">
+                            <Field className="uk-input uk-form-width-large" type="text" placeholder="First Name"
+                                   name="name"/>
+                        </div>
+                        <div className="uk-margin">
+                            <Field className="uk-input uk-form-width-large" type="text" placeholder="Surname"
+                                   name="surname"/>
+                        </div>
 
-                    <div>
-                        <input className="uk-button uk-button-default" type="button" value={this.buttonValue}/>
-                    </div>
-                </fieldset>
-            </form>
+                        <div>
+                            <input className="uk-button uk-button-default" type="submit" value={this.buttonValue}/>
+                        </div>
+                    </fieldset>
+                </Form>
+            </Formik>
         </div>
     }
 }
